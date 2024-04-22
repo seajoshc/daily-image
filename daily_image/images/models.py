9136f7a887de4d2067b5bf8de38d831a729f61a9
Josh Campbell <josh@p4a.net>
2024-04-21 22:19:41 -0700

    adds fields for each image
    only the name is required, date taken defaults to when its uploaded but can be modified. labels are meant to be comma separated and searched on.

--- daily_image/images/models.py
+++ daily_image/images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    date_taken = models.DateField(default=timezone.now)
+    labels = models.CharField(max_length=255, blank=True)
+    location = models.CharField(max_length=255, blank=True)
