4603644d675391eda974f67649b64ac6526f5011
Josh Campbell <josh@p4a.net>
2024-04-23 21:17:52 -0700

    admin first

--- daily_image/urls.py
+++ daily_image/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("images.urls")),
     path("admin/", admin.site.urls),
+    path("", include("images.urls")),
 ]
