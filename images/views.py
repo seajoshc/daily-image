da64322b046ad7d36ca194f6a3b9f017cb6d77ae
Josh Campbell <josh@p4a.net>
2024-04-23 21:33:10 -0700

    updates detail view, pylint

--- images/views.py
+++ images/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Image
+
 
 def index(request):
     # Get image that has is_current_image set to True
     image = Image.objects.filter(is_current_image=True).first()
-    return render(request, 'index.html', {'image': image})
+    return render(request, "index.html", {"image": image})
+
 
 def detail(request, image_id):
-    image = Image.objects.get(id=image_id)
-    return render(request, 'detail.html', {'image': image})
+    image = get_object_or_404(Image, pk=image_id)
+    return render(request, "detail.html", {"image": image})
+
 
 def search(request):
-    query = request.GET['query']
-    images = Image.objects.filter(name__icontains=query) | Image.objects.filter(description__icontains=query) | Image.objects.filter(labels__icontains=query)
-    return render(request, 'search.html', {'images': images})
+    query = request.GET["query"]
+    images = (
+        Image.objects.filter(name__icontains=query)
+        | Image.objects.filter(description__icontains=query)
+        | Image.objects.filter(labels__icontains=query)
+    )
+    return render(request, "search.html", {"images": images})
