8d10439f34014c3c22d5e7945ecaf4f1c59bba9a
Josh Campbell <josh@p4a.net>
2024-04-23 17:19:30 -0700

    use index instead of home, detail instead of image_detail

--- images/views.py
+++ images/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from .models import Image
 
-def home(request):
+def index(request):
     # Get most recent image and display it on the home page
     image = Image.objects.latest('date_taken')
-    return render(request, 'home.html', {'image': image})
+    return render(request, 'index.html', {'image': image})
 
-def image_detail(request, image_id):
+def detail(request, image_id):
     image = Image.objects.get(id=image_id)
-    return render(request, 'image_detail.html', {'image': image})
+    return render(request, 'detail.html', {'image': image})
 
 def search(request):
     query = request.GET['query']
