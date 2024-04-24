8f92170e142491ba78ee9337570f199da252b4c3
Josh Campbell <josh@p4a.net>
2024-04-23 17:17:04 -0700

    use root instead of /images

--- images/urls.py
+++ images/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'images'
+app_name = "images"
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('image/<int:image_id>/', views.image_detail, name='image_detail'),
-    path('search/', views.search, name='search'),
+    path("", views.index, name="index"),
+    path("<int:image_id>/", views.image_detail, name="image_detail"),
+    path("search/", views.search, name="search"),
 ]
